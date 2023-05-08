dcfaecdbb97a1b4d59e88a322a501dec587e9604
Adam Hughes <adamjordanhughes@gmail.com>
2023-05-07 21:08:17 -0400

    Change our file structure to match the medium article

---
+++ src/app/api/generate/route.ts
@@ -0,0 +1,34 @@
+import { OpenAIStream, OpenAIStreamPayload } from "../../utils/openAIStream";
+
+if (!process.env.OPEN_AI_API_KEY) {
+  throw new Error("Missing env var from OpenAI");
+}
+
+export const config = {
+  runtime: "edge",
+};
+
+export async function POST(req: Request): Promise<Response> {
+  const { prompt } = (await req.json()) as {
+    prompt?: string;
+  };
+
+  if (!prompt) {
+    return new Response("No prompt in the request", { status: 400 });
+  }
+
+  const payload: OpenAIStreamPayload = {
+    model: "text-davinci-003",
+    messages: [{ role: "user", content: prompt }],
+    temperature: 0.6,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 2048,
+    stream: true,
+    n: 1,
+  };
+
+  const stream = await OpenAIStream(payload);
+  return new Response(stream);
+}
--- src/app/components/CodeReview.jsx
+++ src/app/components/CodeReview.jsx
@@ -1,7 +1,6 @@
 'use client';
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useDarkMode } from "../contexts/darkMode";
-import InternalApiService from "../services/internalApiService";
 import { toast } from "react-toastify";
 import CodeBlock from "./CodeBlock";
 import CodeReviewResponse from "./CodeReviewResponse";
@@ -26,48 +25,96 @@ const CodeReview = () => {
     setLanguage(e.target.value);
   };
 
-  const handleReview = () => {
+  const handleReview = async () => {
+    let prompt = `You're an expert level software engineer and have a single ${language} file to review. You are reviewing code for peers. You're a stickler for clean code, meaning no comments in the code, code should be self documenting, using correct naming for variables and functions, creating the right abstractions and many more ideas that you pull from Kent Beck, Martin Fowler, Bob Martin and other engineering thought leaders. You do not need to refactor and provide any code unless it makes sense for the small suggestion that you reviewed, note to not do the whole file. Please reply back with only suggestions in markdown format with each suggestion in a list to keep things easy to navigate. Please review the following code now:
+    ${code}`
     if (!code) return toast.error("Please enter some code to review");
     setRefactor("");
     setReview("");
     setIsLoading(true);
-    new InternalApiService()
-      .post("/review", {
-        codeToReview: code,
-        language,
-      })
-      .then((response) => {
-        setIsLoading(false);
-        setReview(response?.result);
-      })
-      .catch((error) => {
-        setIsLoading(false);
-        console.error(error);
-        toast.error(`An error happened: ${error.message}`);
-      });
+
+
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt,
+      }),
+    });
+
+    console.log("Initial response:", response);
+
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    // This data is a ReadableStream
+    const data = response.body;
+    console.log("ReadableStream data:", data);
+
+    if (!data) {
+      return;
+    }
+
+    const reader = data.getReader();
+    const decoder = new TextDecoder();
+    let done = false;
+
+    while (!done) {
+      const { value, done: doneReading } = await reader.read();
+      done = doneReading;
+      const chunkValue = decoder.decode(value);
+      setReview((prev) => prev + chunkValue);
+    }
+    setIsLoading(false);
   };
 
-  const handleRefactor = () => {
+
+  const handleRefactor = async () => {
+    let prompt = `You're an expert level software engineer and have a single ${language} file to refactor. You're a stickler for clean code, meaning no comments in the code, code should be self documenting, using correct naming for variables and functions, creating the right abstractions and many more ideas that you pull from Kent Beck, Martin Fowler, Bob Martin and other engineering thought leaders. Please write inline comments about your design decisions and talking about the choices you made in your refactor (only for things you changed) and reply back the code that you refactored(only put things you actually changed).  Please now refactor the following code:
+    ${code}`
     if (!code) return toast.error("Please enter some code to refactor");
     setRefactor("loading");
     setReview("");
     setIsLoading(true);
-    new InternalApiService()
-      .post("/refactor", {
-        codeToReview: code,
-        language,
-      })
-      .then((response) => {
-        setIsLoading(false);
-        setRefactor(response?.result);
-      })
-      .catch((error) => {
-        setRefactor("");
-        setIsLoading(false);
-        console.error(error);
-        toast.error(`An error happened: ${error.message}`);
-      });
+
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    // This data is a ReadableStream
+    const data = response.body;
+    if (!data) {
+      return;
+    }
+
+    const reader = data.getReader();
+    const decoder = new TextDecoder();
+    let done = false;
+
+    while (!done) {
+      const { value, done: doneReading } = await reader.read();
+      done = doneReading;
+      const chunkValue = decoder.decode(value);
+      setRefactor((prev) => prev + chunkValue);
+    }
+    setIsLoading(false);
+
   };
+
 
   const containerClasses = classNames(
     "container",
