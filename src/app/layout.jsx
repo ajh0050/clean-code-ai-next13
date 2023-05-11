2c72c854e2e4dee6c6b5f5ddadac6fd7fafa64ee
Adam Hughes <adamjordanhughes@gmail.com>
2023-05-11 00:40:35 -0400

    Swap the Head into the the rootlayout component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,40 +3,52 @@ import "./globals.css";
 import { Metadata } from "next";
 import LayoutComponent from "./components/LayoutComponent";
 import { DarkModeProvider } from "./contexts/darkMode";
+import Head from "next/head";
 
-
-
-export const metadata = {
-	title: "Clean Code AI",
-	description: "Get custom tailored code reviews from an opinionated AI.",
-	openGraph: {
-		title: "Clean Code AI",
-		description: "This is a free tool for developers to get their code reviewed or refactored.",
-		url: "https://cleancode-ai.vercel.app/",
-		siteName: "https://cleancode-ai.vercel.app/",
-		images: '/cleancode.png',
-		locale: "en-US",
-		type: "website",
-	  },
-	robots: {
-		index: true,
-		follow: true,
-		googleBot: {
-			index: true,
-			follow: true,
-			"max-video-preview": -1,
-			"max-image-preview": "large",
-			"max-snippet": -1,
-		},
-	},
-	icons: {
-		shortcut: "/favicon.ico",
-	},
-};
+// export const metadata = {
+//   title: "Clean Code AI",
+//   description: "Get custom tailored code reviews from an opinionated AI.",
+//   openGraph: {
+//     title: "Clean Code AI",
+//     description: "This is a free tool for developers to get their code reviewed or refactored.",
+//     url: "https://cleancode-ai.vercel.app/",
+//     siteName: "https://cleancode-ai.vercel.app/",
+//     images: '/cleancode.png',
+//     locale: "en-US",
+//     type: "website",
+//   },
+//   robots: {
+//     index: true,
+//     follow: true,
+//     googleBot: {
+//       index: true,
+//       follow: true,
+//       "max-video-preview": -1,
+//       "max-image-preview": "large",
+//       "max-snippet": -1,
+//     },
+//   },
+//   icons: {
+//     shortcut: "/favicon.ico",
+//   },
+// };
 
 export default function Layout({ children }) {
+  const domain = "https://cleancode-ai.vercel.app";
   return (
     <html lang="en">
+      <Head>
+        <title>Clean Code AI</title>
+        <meta
+          name="description"
+          content="Get custom tailored code reviews from an opinionated AI"
+        />
+        <meta property="og:title" content="Clean Code AI" />
+        <meta property="og:description" content="This is a free tool for developers to get their code reviewed or refactored." />
+        <meta name="image" property="og:image" content={`${domain}/cleancode.png`} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <DarkModeProvider>
           <LayoutComponent>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,18 +7,18 @@ import { Analytics } from '@vercel/analytics/react';
 export default function Home() {
   return (
     <>
-      <Head>
+      {/* <Head>
         <title>Clean Code AI</title>
         <meta
           name="description"
           content="Get custom tailored code reviews from an opinionated AI"
         />
-        {/* <meta property="og:title" content="Clean Code AI" />
+        <meta property="og:title" content="Clean Code AI" />
         <meta property="og:description" content="This is a free tool for developers to get their code reviewed or refactored." />
         <meta name="image" property="og:image" content={`${domain}/cleancode.png`} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" /> */}
-      </Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head> */}
       <main>
         <CodeReview />
         <Analytics />
