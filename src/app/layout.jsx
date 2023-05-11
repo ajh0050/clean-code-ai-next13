516a4cba6a39e7974ac9f83de7445615f28b4665
Adam Hughes <adamjordanhughes@gmail.com>
2023-05-11 00:34:53 -0400

    Add meta data variable in layout and add custom favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,38 @@
 'use client'
 import "./globals.css";
+import { Metadata } from "next";
 import LayoutComponent from "./components/LayoutComponent";
 import { DarkModeProvider } from "./contexts/darkMode";
+
+
+
+export const metadata = {
+	title: "Clean Code AI",
+	description: "Get custom tailored code reviews from an opinionated AI.",
+	openGraph: {
+		title: "Clean Code AI",
+		description: "This is a free tool for developers to get their code reviewed or refactored.",
+		url: "https://cleancode-ai.vercel.app/",
+		siteName: "https://cleancode-ai.vercel.app/",
+		images: '/cleancode.png',
+		locale: "en-US",
+		type: "website",
+	  },
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			"max-video-preview": -1,
+			"max-image-preview": "large",
+			"max-snippet": -1,
+		},
+	},
+	icons: {
+		shortcut: "/favicon.ico",
+	},
+};
 
 export default function Layout({ children }) {
   return (
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,6 @@ import { Analytics } from '@vercel/analytics/react';
 
 
 export default function Home() {
-  const domain = 'https://cleancode-ai.vercel.app/';
   return (
     <>
       <Head>
@@ -14,11 +13,11 @@ export default function Home() {
           name="description"
           content="Get custom tailored code reviews from an opinionated AI"
         />
-        <meta property="og:title" content="Clean Code AI" />
+        {/* <meta property="og:title" content="Clean Code AI" />
         <meta property="og:description" content="This is a free tool for developers to get their code reviewed or refactored." />
         <meta name="image" property="og:image" content={`${domain}/cleancode.png`} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <main>
         <CodeReview />
