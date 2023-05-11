114527f740300ea5f7d1b4d88e51a06171aa1fce
Adam Hughes <adamjordanhughes@gmail.com>
2023-05-11 00:16:34 -0400

    Update OG tags

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,6 +13,9 @@ export default function Home() {
           name="description"
           content="Get custom tailored code reviews from an opinionated AI"
         />
+        <meta property="og:title" content="Clean Code AI" />
+        <meta property="og:description" content="This is a free tool for developers to get their code reviewed or refactored." />
+        <meta name="image" property="og:image" content={`${domain}/cleancode.png`} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
